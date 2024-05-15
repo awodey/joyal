c7d5b797d29477dc7c494a999ec9cede293032b5
Steve Awodey <sawodey@gmail.com>
2024-05-15 16:57:40 +0200

    move comments

--- Joyal/Representation.lean
+++ Joyal/Representation.lean
@@ -1,5 +1,3 @@
--- This module serves as the root of the `Joyal` library.
--- Import modules here that should be built as part of the library.
 import Mathlib.Order.Birkhoff
 import Mathlib.Order.Heyting.Hom
 import Mathlib.Order.Category.BddDistLat
